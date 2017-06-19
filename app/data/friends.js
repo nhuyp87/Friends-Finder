adf41326539165322f6be8283b5aa3dfa01aba25
nhuyp <ypham87@gmail.com>
2017-06-18 18:33:43 -0700

    value of inputs not being logged

--- app/data/friends.js
+++ app/data/friends.js
@@ -15,7 +15,42 @@ var friends =
                 4,
                 1
             ]
+        },
+
+        {
+            "name": "John",
+            "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+            "scores": [
+                1,
+                2,
+                3,
+                4,
+                1,
+                2,
+                3,
+                4,
+                5,
+                1
+            ]
+        },
+
+        {
+            "name": "Joe",
+            "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+            "scores": [
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1
+            ]
         }
+
     ]
 
 var friendModel = {
